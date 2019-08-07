e008c488029310926d4b8f68672da274e3cfdae9
斯周 <m17017309072@163.com>
2019-08-08 00:34:31 +0800

    leetcode25,K个一组反转链表

---
+++ leetcode25.js
@@ -0,0 +1,33 @@
+ var reverseList = function(head) {
+    let pre = null
+    let curr = head
+    let next;
+    while (curr != null) {
+      next = curr.next
+      curr.next = pre;
+      pre = curr;
+      curr = next;
+    }
+    return pre
+};
+var reverseKGroup = function(head, k) {
+    let revList = new ListNode(0)
+    revList.next = head
+    let pre = revList
+    let end = revList
+    let loopCurr = head
+    while (end != null) {
+      for(let i = 0; i < k && end != null; i ++ ) {
+        end = end.next
+      }
+      if (end == null) break;
+      let start = pre.next
+      let next = end.next
+      end.next = null;
+      pre.next = reverseList(start);
+      start.next = next;
+      pre = start;
+      end = pre;
+    }
+    return revList.next
+};
